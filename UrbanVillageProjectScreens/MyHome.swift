be6462f1b405e49f72161d622d5f3ed93339a6d4
Claudia Eng <claudiaeng@live.com>
2020-05-10 01:50:31 -0400

    Update MyHome.swift

--- UrbanVillageProjectScreens/MyHome.swift
+++ UrbanVillageProjectScreens/MyHome.swift
@@ -1,6 +1,6 @@
-//  Copyright © 2020 Studio10011. All rights reserved.
-
 import SwiftUI
+
+// COLORS
 
 let backgroundColor: Color = Color.init(red: 242/255,
                                         green: 240/255,
@@ -43,181 +43,278 @@ let dividerColor: Color = Color.init(red: 71/255,
                                      blue: 87/255)
 
 let opaqueColor: Color = Color.init(red: 85/255,
-green: 87/255,
-blue: 99/255)
+                                    green: 87/255,
+                                    blue: 99/255)
 
 fileprivate func topStack() -> some View {
-    return HStack {
-        Text("My home").kerning(-1).foregroundColor(textColor).font(.custom("HelveticaNeue-Medium", size: 25))
-        
-        Spacer()
-        
-        Image("woman").resizable().scaledToFill().frame(width: 45, height: 45)
-            .clipShape(Circle())
-    }.padding([.horizontal], 20)
+  return HStack {
+    Text("My home").kerning(-1).foregroundColor(textColor).font(.custom("HelveticaNeue-Medium", size: 25))
+    
+    Spacer()
+    
+    Image("woman").resizable().scaledToFill().frame(width: 45, height: 45)
+      .clipShape(Circle())
+  }.padding([.horizontal], 20)
 }
 
 fileprivate func spendCard() -> some View {
-    return //spend card
-        VStack {
-            HStack {
-                Text("Your total monthly spend").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Medium", size: 14))
+  return //spend card
+    VStack {
+      HStack {
+        Text("Your total monthly spend").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Medium", size: 14))
+        
+        Spacer()
+        
+        HStack {
+          Text("March").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Medium", size: 14))
+          Color.clear.frame(width: 0)
+          Image(systemName: "arrowtriangle.down.fill").foregroundColor(accentTextColor).font(.system(size: 9, weight: .black))
+        }.padding([.vertical], 3).padding([.horizontal], 9).overlay(
+          RoundedRectangle(cornerRadius: 20)
+            .stroke(accentTextColor, lineWidth: 1)
+        )
+      }
+      
+      Color.clear.frame(height: 30)
+      
+      HStack {
+        
+        Spacer()
+        
+        Text("1,938").kerning(-0.5).foregroundColor(secondaryColor).font(.custom("HelveticaNeue-Light", size: 55))
+      }
+    }.padding([.all], 15).background(Color.white).cornerRadius(25)
+}
+
+// MARK - main return
+struct MyHome: View {
+  
+  var body: some View {
+    ScrollView {
+      VStack(spacing: 0) {
+        
+        topStack()
+        
+        horizontalScroll()
+        
+        VStack(spacing: 0.0) {
+          
+          spendCard()
+          
+          
+          //other cards
+          VStack(spacing: 0.0) {
+            
+            //first card
+            HStack(alignment: .top) {
+              VStack(alignment: .leading) {
+                Text("Base rent")
+                  .kerning(-0.5)
+                  .foregroundColor(Color.white)
+                  .font(.custom("HelveticaNeue-Regular",
+                                size: 22))
+                Text("1,430")
+                  .kerning(-0.5)
+                  .foregroundColor(firstCardColor)
+                  .brightness(0.15)
+                  .font(.custom("HelveticaNeue-Regular",
+                                size: 22))
+              }
+              
+              Spacer()
+              
+              Image(systemName: "b.circle.fill")
+                .foregroundColor(Color.white.opacity(0.8))
+                .font(.system(size: 35,
+                              weight: .regular))
+                .padding([.top], 5)
+              
+            }.padding([.all], 15)
+              .background(firstCardColor)
+            
+            //second card
+            HStack(alignment: .top) {
+              VStack(alignment: .leading) {
+                Text("Energy")
+                  .kerning(-0.5)
+                  .foregroundColor(Color.white)
+                  .font(.custom("HelveticaNeue-Regular",
+                                size: 22))
+                Text("120")
+                  .kerning(-0.5)
+                  .foregroundColor(secondCardColor)
+                  .brightness(0.15)
+                  .font(.custom("HelveticaNeue-Regular",
+                                size: 22))
+              }
+              
+              Spacer()
+              
+              Image(systemName: "e.circle.fill")
+                .foregroundColor(Color.white.opacity(0.8))
+                .font(.system(size: 35,
+                              weight: .regular))
+                .padding([.top], 5)
+              
+            }.padding([.all], 15)
+              .background(secondCardColor)
+            
+            //third card
+            HStack(alignment: .top) {
+              VStack(alignment: .leading) {
+                Text("Recreation").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 22))
+                Text("78").kerning(-0.5).foregroundColor(thirdCardColor).brightness(0.15).font(.custom("HelveticaNeue-Regular", size: 22))
+              }
+              
+              Spacer()
+              
+              Image(systemName: "r.circle.fill").foregroundColor(Color.white.opacity(0.8)).font(.system(size: 35, weight: .regular)).padding([.top], 5)
+              
+            }.padding([.all], 15).background(thirdCardColor)
+            
+            //fourth card
+            VStack {
+              HStack {
+                VStack(alignment: .leading) {
+                  Text("Food")
+                    .kerning(-0.5)
+                    .foregroundColor(Color.white)
+                    .font(.custom("HelveticaNeue-Regular", size: 22))
+                  Text("23")
+                    .kerning(-0.5)
+                    .foregroundColor(fourthCardColor)
+                    .brightness(0.15)
+                    .font(.custom("HelveticaNeue-Regular", size: 22))
+                }
+                
+                Spacer()
+                
+                Image(systemName: "f.circle.fill").foregroundColor(Color.white.opacity(0.8)).font(.system(size: 35,
+                                                                                                          weight: .regular))
+                  .padding([.top], 5)
+              }.padding([.bottom], 50)
+              
+              dividerColor.frame(height: 2)
+              
+              HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                  Text("Your meal box")
+                    .kerning(-0.5)
+                    .foregroundColor(Color.white)
+                    .font(.custom("HelveticaNeue-Regular",
+                                  size: 16))
+                  Text("Vegetarian").kerning(-0.5)                    .foregroundColor(fourthCardColor)
+                    .brightness(0.15)
+                    .font(.custom("HelveticaNeue-Regular",
+                                  size: 14))
+                }
+                
+                VStack(alignment: .leading) {
+                  Text("People")
+                    .kerning(-0.5)
+                    .foregroundColor(Color.white)
+                    .font(.custom("HelveticaNeue-Regular", size: 16))
+                  Text("2")
+                    .kerning(-0.5)
+                    .foregroundColor(fourthCardColor)
+                    .brightness(0.15)
+                    .font(.custom("HelveticaNeue-Regular", size: 14))
+                }
+                
+                VStack(alignment: .leading) {
+                  Text("Days")
+                    .kerning(-0.5)
+                    .foregroundColor(Color.white)
+                    .font(.custom("HelveticaNeue-Regular", size: 16))
+                  Text("5")
+                    .kerning(-0.5)
+                    .foregroundColor(fourthCardColor)
+                    .brightness(0.15)                    .font(.custom("HelveticaNeue-Regular", size: 14))
+                }
                 
                 Spacer()
                 
                 HStack {
-                    Text("March").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Medium", size: 14))
-                    Color.clear.frame(width: 0)
-                    Image(systemName: "arrowtriangle.down.fill").foregroundColor(accentTextColor).font(.system(size: 9, weight: .black))
-                }.padding([.vertical], 3).padding([.horizontal], 9).overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(accentTextColor, lineWidth: 1)
-                )
-            }
-            
-            Color.clear.frame(height: 30)
-            
-            HStack {
+                  Text("Change plan").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Medium", size: 14))
+                }.padding([.vertical], 2).padding([.horizontal], 7).background(opaqueColor.opacity(0.9)).cornerRadius(10)
+              }.padding([.bottom], 10)
+              
+              dividerColor.frame(height: 2)
+              
+              HStack {
+                VStack(alignment: .leading) {
+                  Text("Next Delivery")
+                    .kerning(-0.5)
+                    .foregroundColor(Color.white)
+                    .font(.custom("HelveticaNeue-Regular", size: 16))
+                  Text("Monday 4th 4pm")
+                    .kerning(-0.5)
+                    .foregroundColor(fourthCardColor)
+                    .brightness(0.15)
+                    .font(.custom("HelveticaNeue-Regular", size: 14))
+                }
                 
                 Spacer()
                 
-                Text("1,938").kerning(-0.5).foregroundColor(secondaryColor).font(.custom("HelveticaNeue", size: 55))
-            }
-        }.padding([.all], 15).background(Color.white).cornerRadius(25)
-}
-
-struct MyHome: View {
-    
-    var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                
-                topStack()
-                
-                // subs
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        Color.clear.frame(width: 13)
-                        
-                        Text("Subscriptions").kerning(-0.5).foregroundColor(secondaryColor).font(.custom("HelveticaNeue-Medium", size: 18)).padding([.vertical], 5).padding([.horizontal], 10).background(Color.white).cornerRadius(15)
-                        
-                        Color.clear.frame(width: 90)
-                        
-                        Text("Utilities").kerning(-0.5).foregroundColor(secondaryAccentTextColor).font(.custom("HelveticaNeue-Medium", size: 18))
-                        
-                        Color.clear.frame(width: 20)
-                        
-                        Text("Finances").kerning(-0.5).foregroundColor(secondaryAccentTextColor).font(.custom("HelveticaNeue-Medium", size: 18))
-                    }
-                }.padding([.bottom], 20).padding([.top], 15)
-                
-                VStack(spacing: 0.0) {
-                    
-                    spendCard()
-                    
-                    //other cards
-                    VStack(spacing: 0.0) {
-                        
-                        //first card
-                        HStack(alignment: .top) {
-                            VStack(alignment: .leading) {
-                                Text("Base rent").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 22))
-                                Text("1,430").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 22))
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "b.circle.fill").foregroundColor(Color.white.opacity(0.8)).font(.system(size: 35, weight: .regular)).padding([.top], 5)
-                            
-                        }.padding([.all], 15).background(firstCardColor)
-                        
-                        HStack(alignment: .top) {
-                            VStack(alignment: .leading) {
-                                Text("Energy").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 22))
-                                Text("120").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 22))
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "e.circle.fill").foregroundColor(Color.white.opacity(0.8)).font(.system(size: 35, weight: .regular)).padding([.top], 5)
-                            
-                        }.padding([.all], 15).background(secondCardColor)
-                        
-                        HStack(alignment: .top) {
-                            VStack(alignment: .leading) {
-                                Text("Recreation").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 22))
-                                Text("78").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 22))
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "r.circle.fill").foregroundColor(Color.white.opacity(0.8)).font(.system(size: 35, weight: .regular)).padding([.top], 5)
-                            
-                        }.padding([.all], 15).background(thirdCardColor)
-                        
-                        VStack {
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text("Food").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 22))
-                                    Text("23").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 22))
-                                }
-                                
-                                Spacer()
-                                
-                                Image(systemName: "f.circle.fill").foregroundColor(Color.white.opacity(0.8)).font(.system(size: 35, weight: .regular)).padding([.top], 5)
-                            }
-                            
-                            dividerColor.frame(height: 2)
-                            
-                            HStack(alignment: .top) {
-                                VStack(alignment: .leading) {
-                                    Text("Your meal box").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 16))
-                                    Text("Vegetarian").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 14))
-                                }
-                                
-                                VStack(alignment: .leading) {
-                                    Text("People").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 16))
-                                    Text("2").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 14))
-                                }
-                                
-                                VStack(alignment: .leading) {
-                                    Text("Days").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 16))
-                                    Text("5").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 14))
-                                }
-                                
-                                Spacer()
-                                
-                                HStack {
-                                    Text("Change plan").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Medium", size: 14))
-                                    }.padding([.vertical], 2).padding([.horizontal], 7).background(opaqueColor.opacity(0.9)).cornerRadius(10)
-                            }.padding([.bottom], 10)
-                            
-                            dividerColor.frame(height: 2)
-                            
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text("Next Delivery").kerning(-0.5).foregroundColor(Color.white).font(.custom("HelveticaNeue-Regular", size: 16))
-                                    Text("Monday 4th 4pm").kerning(-0.5).foregroundColor(accentTextColor).font(.custom("HelveticaNeue-Regular", size: 14))
-                                }
-                                
-                                Spacer()
-                                
-                            }.padding([.bottom], 10)
-                            
-                        }.padding([.all], 15).background(fourthCardColor)
-                        
-                    }.cornerRadius(25)
-                }.padding([.horizontal], 20)
-                
-            }.padding([.top], 75)
+              }.padding([.bottom], 10)
+              
+            }.padding([.all], 15).background(fourthCardColor)
             
             
-        }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height).background(backgroundColor)
-    }
+          }.cornerRadius(25)
+        }.padding([.horizontal], 20)
+        
+      }.padding([.top], 75)
+      
+      
+    }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height).background(backgroundColor)
+  }
 }
 
 struct MyHome_Previews: PreviewProvider {
-    static var previews: some View {
-        MyHome()
-    }
+  static var previews: some View {
+    MyHome()
+  }
+}
+
+struct horizontalScrollState {
+  @State var selectedTab = true
+}
+
+fileprivate func horizontalScroll() -> some View {
+  
+  return // top horizontal scroll
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack {
+        Color.clear.frame(width: 13)
+        
+        VStack {
+          Text("Subscriptions")
+            .kerning(-0.5)
+            .foregroundColor(secondaryColor)
+            .font(.custom("HelveticaNeue-Medium", size: 18))
+            .padding([.vertical], 5)
+            .padding([.horizontal], 10)
+            .background(Color.white)
+            .cornerRadius(15)
+        }.onTapGesture {
+          print("tapped")
+        }
+        
+        Color.clear.frame(width: 90)
+        
+        Text("Utilities").kerning(-0.5)
+          .foregroundColor(secondaryAccentTextColor)
+          .font(.custom("HelveticaNeue-Medium",
+                        size: 18))
+        
+        Color.clear.frame(width: 20)
+        
+        Text("Finances")
+          .kerning(-0.5)
+          .foregroundColor(secondaryAccentTextColor)
+          .font(.custom("HelveticaNeue-Medium",
+                        size: 18))
+      }
+    }.padding([.bottom], 20).padding([.top], 15)
 }
